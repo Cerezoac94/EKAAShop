951a3a399135381dd1878b91eb673bb4e1f066b7
Ana Sanchez <ana.sanchezcvr@gmail.com>
2023-02-27 11:19:54 -0600

    feature/created-login/register-components

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import Login from "../components/forms/Login";
+import Register from "../components/forms/Register";
 
 const router = createBrowserRouter([
 	{
@@ -39,7 +41,11 @@ const router = createBrowserRouter([
 	},
 	{
 		path: "/login",
-		element: "componente de logIn",
+		element: <Login/>
+	},
+	{
+		path: "/register",
+		element: <Register/>
 	},
 ]); // va treer las rutas, es un array de rutas(obbjets)
 
