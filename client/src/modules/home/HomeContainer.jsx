855d195a124797926b6ab48f235bf7f19381ac1d
EugeniaRS <kenayanrkt@gmail.com>
2023-03-03 13:58:52 -0600

    add(adding-styles):add styles

--- client/src/modules/home/HomeContainer.jsx
+++ client/src/modules/home/HomeContainer.jsx
@@ -11,7 +11,7 @@ const HomeContainer = () => {
     <section>
       <SlideHomeMap/>
       <Warranty/>
-      <Warranty/>
+    
       <InterestYouMap/>
       <BannerOffersMap/>
       <ReviewsClientMap/>
