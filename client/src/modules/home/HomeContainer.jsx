0cd12e6ef0c24da414f4e6438b0346c10629d715
Jorge Cerezo <antonio.cerezo94@hotmail.com>
2023-03-07 13:51:13 -0600

    fix(client-modules): Fix component keepBuying

--- client/src/modules/home/HomeContainer.jsx
+++ client/src/modules/home/HomeContainer.jsx
@@ -1,7 +1,7 @@
 import BannerOffersMap from "./components/bannerOffers/BannerOffersMap"
 import InterestYouMap from "./components/interestYou/InterestYouMap"
 import KeepBuyingMap from "./components/keepBuying_buyAgain/KeepBuyingMap"
-import BuyAgainMap from "./components/keepBuying_buyAgain/BuyAgainMap"
+// import BuyAgainMap from "./components/keepBuying_buyAgain/BuyAgainMap"
 import ReviewsClientMap from "./components/reviewsClient/ReviewsClientMap"
 import SlideHomeMap from "./components/slideHome/SlideHomeMap"
 import Warranty from "./components/warranty/Warranty"
@@ -16,7 +16,7 @@ const HomeContainer = () => {
       <BannerOffersMap/>
       <ReviewsClientMap/>
       <KeepBuyingMap me={1}/>
-      <BuyAgainMap me={1}/>
+      {/* <BuyAgainMap me={1}/> */}
      
 
     </>
--- client/src/modules/home/components/keepBuying_buyAgain/KeepBuyingMap.jsx
+++ client/src/modules/home/components/keepBuying_buyAgain/KeepBuyingMap.jsx
@@ -3,16 +3,19 @@ import KeepBuying from "./KeepBuying"
 
 const KeepBuyingMap = ({ me }) => {
     const { data:results=[], isLoading, error } = useGetWishQuery(me);
-    console.log(results.results);
+    // console.log(results?.results?.Products)
   return isLoading ? ( <h3>Cargando...</h3>
   ):(
+    
+    results.results.Products.length > 0 &&(
    <section>
    <h1>KeepBuying</h1>
+
     <section className="keepBuyingContainer">
     
     {results.results.Products.slice(0,4).map(product => <KeepBuying product={product} key={product.id} />)}
     </section>
-    </section>
+    </section>)
   )
 }
 
