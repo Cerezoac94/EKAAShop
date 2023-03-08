75e6104b97d17a5957cc6c657f4473a9a5987177
KevinConde <conde.jimenez@hotmail.com>
2023-03-07 19:05:31 -0600

    fix(api-controllers): Create-deleteOrder-deleteProductOrder-controller

--- api/src/controllers/order/Order.controller.js
+++ api/src/controllers/order/Order.controller.js
@@ -1,4 +1,14 @@
-import { Order, OrderDetail, Product, Cart, CartProduct, Discount, User, Category } from "../../models/index.js";
+import { Op, Sequelize } from "sequelize";
+import {
+  Order,
+  OrderDetail,
+  Product,
+  Cart,
+  CartProduct,
+  Discount,
+  User,
+  Category,
+} from "../../models/index.js";
 
 class OrderController {
   //CREATE ORDER
@@ -11,20 +21,22 @@ class OrderController {
           idUser: id,
         },
       });
-      if (!cart) throw "Error finding cart"
+      if (!cart) throw "Error finding cart";
       // guardar detalle del carrito
       const cartProducts = await CartProduct.findAll({
         where: {
           idCart: cart.id,
         },
       });
-      console.log('cartProduct',cartProducts);
-      if (!cartProducts.length) throw "There are no products to add to the order";
+      console.log("cartProduct", cartProducts);
+      if (!cartProducts.length)
+        throw "There are no products to add to the order";
       // comprobar stock suficiente
       // Verificamos si los productos tienen suficiente stock
       for (const cartProduct of cartProducts) {
         const product = await Product.findByPk(cartProduct.idProduct);
-        if (parseInt(product.stock) < parseInt(cartProduct.quantity)) throw "Uno o más productos no tienen suficiente stock";
+        if (parseInt(product.stock) < parseInt(cartProduct.quantity))
+          throw "Uno o más productos no tienen suficiente stock";
       }
       // Creamos la orden
       const order = await Order.create({
@@ -36,7 +48,7 @@ class OrderController {
       if (!order) throw "The order is not created";
 
       // Calcular total y creación del detalle de la orden
-      let total = 0
+      let total = 0;
       for (const cartProduct of cartProducts) {
         const product = await Product.findByPk(cartProduct.idProduct);
         let productPrice = product.price;
@@ -58,13 +70,13 @@ class OrderController {
           idOrder: order.id,
           idProduct: cartProduct.idProduct,
           quantity: cartProduct.quantity,
-          unitPrice: productPrice
+          unitPrice: productPrice,
         });
         await product.update({
           stock: parseInt(product.stock) - parseInt(cartProduct.quantity),
         });
 
-        total += (parseInt(productPrice) * parseInt(cartProduct.quantity))
+        total += parseInt(productPrice) * parseInt(cartProduct.quantity);
       }
 
       // Eliminar productos del carrito
@@ -96,7 +108,7 @@ class OrderController {
           // NumOrden, Quien la hizo, fecha Orden, total de la order, status
           {
             model: OrderDetail,
-            attributes: ['idProduct', 'quantity', 'unitPrice'],
+            attributes: ["idProduct", "quantity", "unitPrice"],
             include: [
               {
                 model: Product,
@@ -271,13 +283,53 @@ class OrderController {
   //Desde vista de lista o general de las ordenes
   static async deleteOrder(req, res) {
     try {
-      const  { idOrder } = req.params;
-      const results = await Order.destroy({
+      const { idOrder } = req.params;
+
+      //Encontramos la orden
+      const order = await Order.findOne({
         where: {
           id: idOrder,
         },
       });
-      if (results === 0) throw "No order was deleted";
+      //Con la orden encontrada se valida el shipmentState
+      if (order.shipmentState === "enviado") {
+        throw new Error("Only can cancel orders with a 'no enviado' status");
+      }
+
+      //Se busca el detalle de la orden
+      const orderDetail = await OrderDetail.findAll({
+        where: {
+          idOrder: idOrder,
+        },
+      });
+
+      //Se itera sobre la orderDetail
+      for (const detail of orderDetail) {
+        //En la ordenDetail se encuentra el producto a afectar
+        const producto = await Product.findOne({
+          where: {
+            id: detail.idProduct,
+          },
+        });
+        // se afecta el stock del producto
+        await Product.update(
+          {
+            stock: detail.quantity + producto.stock,
+          },
+          {
+            where: {
+              id: detail.idProduct,
+            },
+          }
+        );
+      }
+
+      await Order.destroy({
+        where: {
+          id: idOrder,
+        },
+      });
+
       res.status(201).send({
         success: true,
         message: "Order deleted",
@@ -285,15 +337,79 @@ class OrderController {
     } catch (err) {
       res.status(404).send({
         success: false,
-        message: err,
+        message: err.message,
       });
     }
   }
 
   // desde el detalle de la orden
-  static async deleteProductOrder(req, res){
-// idOrder y idProduct desde params
-// quantity desde body
+  static async deleteProductOrder(req, res) {
+    // idOrder y idProduct desde params
+    // quantity desde body
+    try {
+      const { idOrder, idProduct } = req.params;
+      const { quantity } = req.body;
+      const qty = parseInt(quantity);
+
+      const order = await OrderDetail.findOne({
+        where: {
+          idorder: idOrder,
+          [Op.and]: [{ idProduct: idProduct }],
+        },
+      });
+      if (quantity > order.quantity) {
+        throw new Error(
+          "The quantity is greater than the one registered in the order"
+        );
+      }
+      const product = await Product.findOne({
+        where: {
+          id: order.idProduct,
+        },
+      });
+
+      await product.update(
+        {
+          stock: qty + product.stock,
+        },
+        {
+          where: {
+            id: product.id,
+          },
+        }
+      );
+
+      await OrderDetail.update(
+        {
+          quantity: order.quantity - qty,
+        },
+        {
+          where: {
+            idorder: idOrder,
+            [Op.and]: [{ idProduct: idProduct }],
+          },
+        }
+      );
+
+      await OrderDetail.destroy({
+        where: {
+          idProduct: idProduct,
+          quantity: {
+            [Op.eq]: 0,
+          },
+        },
+      });
+
+      res.status(201).send({
+        success: true,
+        message: "Item deleted",
+      });
+    } catch (err) {
+      res.status(404).send({
+        success: false,
+        message: err.message,
+      });
+    }
   }
 }
 export default OrderController;
