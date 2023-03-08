46896b579d43c5b08ec2852a145eda6ac1adff4e
EugeniaRS <kenayanrkt@gmail.com>
2023-03-08 17:06:11 -0600

    feature(creacion-estilos):estilos-shop

--- client/src/modules/shoppinCart/components/listCart/ListCart.jsx
+++ client/src/modules/shoppinCart/components/listCart/ListCart.jsx
@@ -1,56 +1,94 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
+import img_stanley from '../../../../assets/stanley_tumbler.jpg'
 
 const ListCart = () => {
   return (
     <section className="listCart">
-      <Card border="primary" className="card">
-        <Card.Body>
-          <Card.Text>
-            <h2>
-              Subtotal: <span>$980.00</span>
-            </h2>
-            <div>
-              {" "}
-              <button className="listCart__btn">Proceed to checkout </button>
-            </div>
-          </Card.Text>
-        </Card.Body>
-      </Card>
 
-      <Card border="primary" className="card">
-        <Card.Header>Header</Card.Header>
-
-        <Card.Body>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          {/*   <Card.Title>Primary Card Title</Card.Title> */}
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer className="card__footer">
-          <div className="card__containerBtn">
-            <button>-</button>
-            <input type="text" />
-            <button>+</button>
+      <section className="listCart__card">
+        <article className="listCart__contText">
+          <h2 className="listCart__h2">
+            Subtotal: <span className="listCart__span">$980.00</span>
+          </h2>
+          <div className="listCart__div">
+            <button className="listCart__btn">Proceed to checkout </button>
+          </div>
+        </article>
+      </section>
+      
+      <section className="listCart_container">
+      <section className="listCart__cardTwo">
+        <article className="listCart__contenedorTxt">
+          <div className='listCart__contimg'>
+          <img src={img_stanley} className='listCart__img' alt="" />
+          </div>
+         
+          <div className="listCart__text">
+            <h2 className="listCart__confirmation">Added to cart</h2>
+            <span className="listCart__desc">Vaso Térmico de Acero Inoxidable, Taza Termica Frio y Caliente</span>
+            <span className="listCart__price">$950.00</span>
+            <small className="listCart__stock">In stock</small>
+            <label className="listCart__price">Color: <span className="listCart__color">Rosa Navy</span></label>
+            <label className="listCart__price">Size: <span className="listCart__color">1 count</span></label>
+          </div>
+        </article>
+        <article className="listCart__footer">
+          <div className="listCart__contBtn">
+            <button className="listCart__btnIcon"><ion-icon name="remove-outline"></ion-icon></button>
+            <input className="listCart__inputIcon" type="text" />
+            <button className="listCart__btnIcon"><ion-icon name="add-outline"></ion-icon></button>
           </div>
 
-          <div className="card__btn">
+          <div className="listCart__contBtnTwo">
             <div>
-              {" "}
-              <button className="btn">delete</button>
+              <button className="listCart__btnThird">Delete</button>
             </div>
             <div>
-              <button className="btn">Safe for later</button>
+              <button className="listCart__btnThirdA">Safe for later</button>
             </div>
           </div>
+        </article>
 
-          {/* 
-          <small className="text-muted">Last updated 3 mins ago</small> */}
-        </Card.Footer>
-      </Card>
+      </section>
+      <section className="listCart__cardTwo">
+        <article className="listCart__contenedorTxt">
+          <div className='listCart__contimg'>
+          <img src={img_stanley} className='listCart__img' alt="" />
+          </div>
+         
+          <div className="listCart__text">
+            <h2 className="listCart__confirmation">Added to cart</h2>
+            <span className="listCart__desc">Vaso Térmico de Acero Inoxidable, Taza Termica Frio y Caliente</span>
+            <span className="listCart__price">$950.00</span>
+            <small className="listCart__stock">In stock</small>
+            <label className="listCart__price">Color: <span className="listCart__color">Rosa Navy</span></label>
+            <label className="listCart__price">Size: <span className="listCart__color">1 count</span></label>
+          </div>
+        </article>
+        <article className="listCart__footer">
+          <div className="listCart__contBtn">
+            <button className="listCart__btnIcon"><ion-icon name="remove-outline"></ion-icon></button>
+            <input className="listCart__inputIcon" type="text" />
+            <button className="listCart__btnIcon"><ion-icon name="add-outline"></ion-icon></button>
+          </div>
+
+          <div className="listCart__contBtnTwo">
+            <div>
+              <button className="listCart__btnThird">Delete</button>
+            </div>
+            <div>
+              <button className="listCart__btnThirdA">Safe for later</button>
+            </div>
+          </div>
+        </article>
+
+      </section>
+      
+      </section>
+    
+      
+      {/*   <h3 className="listCart__h3condicional"><ion-icon name="checkmark-circle-outline"></ion-icon> Tu pedido contiene envio GRATIS </h3> */}
+      
     </section>
   );
 };
