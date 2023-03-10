3da6458a689f7bd1666b249ccba650a6cf86c786
EugeniaRS <kenayanrkt@gmail.com>
2023-03-10 11:26:28 -0600

    feature(update):errores

--- client/src/modules/home/components/reviewsClient/ReviewsClientMap.jsx
+++ client/src/modules/home/components/reviewsClient/ReviewsClientMap.jsx
@@ -5,11 +5,25 @@ import {useGetAllReviewsQuery} from '../../../../redux/service/review.service'
 
 const ReviewsClientMap = () => {
 /* const {data:products, isLoading}=useGetAllProductsQuery(); */
-const {data:products=[], isLoading }= useGetAllReviewsQuery();
+const {data:products=[], isLoading, error }= useGetAllReviewsQuery();
+console.log(products)
 /* products?.results.slice(0,4).map( (e)  => {
  console.log(e)
 }) */  
-  return (isLoading ? (<h2> cargando</h2>) :   (
+
+/* return error ? (<h3>{error?.data?.message}</h3>) :(
+
+isLoading ? (
+    <h3>Cargando...</h3>):(
+    results.length != 0 && (results.results.map(category => (
+      <Category category={category} key={category.id} />)))
+      )) */
+
+
+
+  return error ? (<h3> {error?.data?.message}</h3>) :
+  
+  (isLoading ? (<h2> cargando</h2>) :   (
     products?.results.slice(0,1).map( (e , id)  => {
      return (<ReviewsClient e={e} key={id}/>)
      })  
