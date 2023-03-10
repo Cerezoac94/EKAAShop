1530a9914826fa495e96e68820f1cece7721a4a1
EugeniaRS <kenayanrkt@gmail.com>
2023-03-10 15:27:14 -0600

    add/(update):swiper

--- client/src/modules/home/components/interestYou/InterestYouSwiper.jsx
+++ client/src/modules/home/components/interestYou/InterestYouSwiper.jsx
@@ -3,17 +3,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper";
+import InterestYouMap from "./InterestYouMap";
  
-import Container from "react-bootstrap/esm/Container";
-
-import img from "../../../../assets/stanley_tumbler.svg";
-
-const InterestYouSwiper = () => {
+const InterestYouSwiper = ({e}) => {
   
-    
+    console.log(e)
   return (
     <section>
-      InterestYouSwiper
+    {/*   InterestYouSwiper */}
       <Swiper
      slidesPerView={1}
      spaceBetween={10}
@@ -42,36 +39,11 @@ const InterestYouSwiper = () => {
      className="mySwiper"
     
       >
-        <Container>
-          <SwiperSlide>
-            <section className="interestYou">
-              <article className="interestYou__description">
-                <div>
-                  <img className="interestYou__img" src={img} alt="product" />
-                </div>
 
-                <div className="interestYou__text">
-                  <label htmlFor="name" className="interestYou__label">
-                    Termo Yeti 30 Oz.
-                  </label>
-                  <span className="interestYou__priceItem">
-                    Acero Inoxidable Rosa
-                  </span>
-                  {""}
 
-                  <span className="interestYou__priceItem">$1500.00</span>
-                  <span className="interestYou__stockitem">In Stock</span>
-                  <div>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                  </div>
-                </div>
-              </article>
-            </section>
-          </SwiperSlide>
-          <SwiperSlide>
+         <InterestYouMap/>
+        
+ {/*          <SwiperSlide>
           <section className="interestYou">
               <article className="interestYou__description">
                 <div>
@@ -98,65 +70,11 @@ const InterestYouSwiper = () => {
                 </div>
               </article>
             </section>
-          </SwiperSlide>
-          <SwiperSlide>
-          <section className="interestYou">
-              <article className="interestYou__description">
-                <div>
-                  <img className="interestYou__img" src={img} alt="product" />
-                </div>
+            </SwiperSlide>
+ */}
 
-                <div className="interestYou__text">
-                  <label htmlFor="name" className="interestYou__label">
-                    Termo Yeti 30 Oz.
-                  </label>
-                  <span className="interestYou__priceItem">
-                    Acero Inoxidable Rosa
-                  </span>
-                  {""}
-
-                  <span className="interestYou__priceItem">$1500.00</span>
-                  <span className="interestYou__stockitem">In Stock</span>
-                  <div>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                  </div>
-                </div>
-              </article>
-            </section>
-          </SwiperSlide>
-          <SwiperSlide>
-          <section className="interestYou">
-              <article className="interestYou__description">
-                <div>
-                  <img className="interestYou__img" src={img} alt="product" />
-                </div>
-
-                <div className="interestYou__text">
-                  <label htmlFor="name" className="interestYou__label">
-                    Termo Yeti 30 Oz.
-                  </label>
-                  <span className="interestYou__priceItem">
-                    Acero Inoxidable Rosa
-                  </span>
-                  {""}
-
-                  <span className="interestYou__priceItem">$1500.00</span>
-                  <span className="interestYou__stockitem">In Stock</span>
-                  <div>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                    <ion-icon name="star"></ion-icon>
-                  </div>
-                </div>
-              </article>
-            </section>
-          </SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-        </Container>
+{/*           <SwiperSlide>Slide 5</SwiperSlide> */}
+       {/*  </Container> */}
       </Swiper>
     </section>
   );
