3e72c41ae6d61e144e604bc5bb7494d3d80542aa
Ana Sanchez <ana.sanchezcvr@gmail.com>
2023-03-10 20:48:21 -0600

    destroying the header

--- client/src/modules/home/components/keepBuying_buyAgain/KeepBuyingMap.jsx
+++ client/src/modules/home/components/keepBuying_buyAgain/KeepBuyingMap.jsx
@@ -4,7 +4,8 @@ import KeepBuying from "./KeepBuying"
 const KeepBuyingMap = ({ me }) => {
     const { data:results=[], isLoading, error } = useGetWishQuery(me);
     // console.log(results?.results?.Products)
-  return isLoading ? ( <h3>Cargando...</h3>
+    return error ? (<h3>{error?.data?.message}</h3>) :(
+  isLoading ? ( <h3>Cargando...</h3>
   ):(
     
     results.results.Products.length > 0 &&(
@@ -16,7 +17,7 @@ const KeepBuyingMap = ({ me }) => {
     {results.results.Products.slice(0,4).map(product => <KeepBuying product={product} key={product.id} />)}
     </section>
     </section>)
-  )
+  ))
 }
 
 export default KeepBuyingMap
