1555404a4e28b04e7e14cd62c00a9c1c9f36f5c2
Jorge Cerezo <antonio.cerezo94@hotmail.com>
2023-03-12 15:30:34 -0600

    add(client-admin): Delete product

--- client/src/modules/admin/components/product/mutationProduct/DeleteProduct.jsx
+++ client/src/modules/admin/components/product/mutationProduct/DeleteProduct.jsx
@@ -1,6 +1,29 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDeleteProductMutation } from "../../../../../redux/service/product.service";
+import Swal from "sweetalert2";
+
 const DeleteProduct = ({ product }) => {
-  return (
-    <div>DeleteProduct</div>
-  )
-}
-export default DeleteProduct
+	const data = useDeleteProductMutation();
+	const navigate = useNavigate();
+	console.log(data);
+	return (
+		<section className="mutation__container">
+			<h3 className="prop__title">Eliminar Producto</h3>
+			<section>
+				<section className="mutation__name">
+					<li>{`Id: ${product?.id}`}</li>
+					<li>{`Name: ${product?.name}`}</li>
+				</section>
+			</section>
+
+			<section className="mutation__submit">
+				<button onClick={() => handleDelete(category.id)}
+					className="btn">
+					Eliminar
+				</button>
+			</section>
+		</section>
+	);
+};
+export default DeleteProduct;
