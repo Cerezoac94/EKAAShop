c7186b87d03f51028934f81e9198413e91bd1f11
EugeniaRS <kenayanrkt@gmail.com>
2023-03-12 19:06:18 -0600

    refactor(estilos):breakpoints

--- client/src/modules/shoppinCart/components/listCart/ListCart.jsx
+++ client/src/modules/shoppinCart/components/listCart/ListCart.jsx
@@ -1,94 +1,53 @@
 import React from "react";
-import img_stanley from '../../../../assets/stanley_tumbler.jpg'
+import img_stanley from "../../../../assets/stanley_tumbler.jpg";
 
 const ListCart = () => {
   return (
     <section className="listCart">
-
-      <section className="listCart__card">
-        <article className="listCart__contText">
+      <section>
+        <article className=" listCart__card">
           <h2 className="listCart__h2">
             Subtotal: <span className="listCart__span">$980.00</span>
           </h2>
-          <div className="listCart__div">
-            <button className="listCart__btn">Proceed to checkout </button>
-          </div>
+          <button className="listCart__btn">Proceed to checkout </button>
         </article>
       </section>
-      
-      <section className="listCart_container">
-      <section className="listCart__cardTwo">
-        <article className="listCart__contenedorTxt">
-          <div className='listCart__contimg'>
-          <img src={img_stanley} className='listCart__img' alt="" />
-          </div>
-         
-          <div className="listCart__text">
-            <h2 className="listCart__confirmation">Added to cart</h2>
-            <span className="listCart__desc">Vaso Térmico de Acero Inoxidable, Taza Termica Frio y Caliente</span>
-            <span className="listCart__price">$950.00</span>
-            <small className="listCart__stock">In stock</small>
-            <label className="listCart__price">Color: <span className="listCart__color">Rosa Navy</span></label>
-            <label className="listCart__price">Size: <span className="listCart__color">1 count</span></label>
-          </div>
-        </article>
-        <article className="listCart__footer">
-          <div className="listCart__contBtn">
-            <button className="listCart__btnIcon"><ion-icon name="remove-outline"></ion-icon></button>
-            <input className="listCart__inputIcon" type="text" />
-            <button className="listCart__btnIcon"><ion-icon name="add-outline"></ion-icon></button>
-          </div>
 
-          <div className="listCart__contBtnTwo">
-            <div>
-              <button className="listCart__btnThird">Delete</button>
+      <section className="listCart__allContainer">
+       {/*  <section> */}
+          <article className="listCart__containerProd">
+            <div className="listCart__contimg">
+              <img src={img_stanley} className="listCart__img" alt="" />
+              <div className="listCart__contBtn">
+                <button className="listCart__btnIcon">
+                  <ion-icon name="remove-outline"></ion-icon>
+                </button>
+                <input className="listCart__inputIcon" type="text" />
+                <button className="listCart__btnIcon">
+                  <ion-icon name="add-outline"></ion-icon>
+                </button>
+              </div>
             </div>
-            <div>
-              <button className="listCart__btnThirdA">Safe for later</button>
+            <div className="listCart__text">
+              <h2 className="listCart__confirmation">Added to cart</h2>
+              <span className="listCart__desc">
+                Vaso Térmico de Acero Inoxidable, Taza Termica Frio y Caliente
+              </span>
+              <span className="listCart__price">$950.00</span>
+              <small className="listCart__stock">In stock</small>
+              <label className="listCart__price">
+                Color: <span className="listCart__color">Rosa Navy</span>
+              </label>
+              <label className="listCart__price">
+                Size: <span className="listCart__color">1 count</span>
+              </label>
+              <div className="listCart__contBtnThird">
+                <button className="listCart__btnThird">Delete</button>
+              </div>
             </div>
-          </div>
-        </article>
-
+          </article>
+   {/*      </section> */}
       </section>
-      <section className="listCart__cardTwo">
-        <article className="listCart__contenedorTxt">
-          <div className='listCart__contimg'>
-          <img src={img_stanley} className='listCart__img' alt="" />
-          </div>
-         
-          <div className="listCart__text">
-            <h2 className="listCart__confirmation">Added to cart</h2>
-            <span className="listCart__desc">Vaso Térmico de Acero Inoxidable, Taza Termica Frio y Caliente</span>
-            <span className="listCart__price">$950.00</span>
-            <small className="listCart__stock">In stock</small>
-            <label className="listCart__price">Color: <span className="listCart__color">Rosa Navy</span></label>
-            <label className="listCart__price">Size: <span className="listCart__color">1 count</span></label>
-          </div>
-        </article>
-        <article className="listCart__footer">
-          <div className="listCart__contBtn">
-            <button className="listCart__btnIcon"><ion-icon name="remove-outline"></ion-icon></button>
-            <input className="listCart__inputIcon" type="text" />
-            <button className="listCart__btnIcon"><ion-icon name="add-outline"></ion-icon></button>
-          </div>
-
-          <div className="listCart__contBtnTwo">
-            <div>
-              <button className="listCart__btnThird">Delete</button>
-            </div>
-            <div>
-              <button className="listCart__btnThirdA">Safe for later</button>
-            </div>
-          </div>
-        </article>
-
-      </section>
-      
-      </section>
-    
-      
-      {/*   <h3 className="listCart__h3condicional"><ion-icon name="checkmark-circle-outline"></ion-icon> Tu pedido contiene envio GRATIS </h3> */}
-      
     </section>
   );
 };
