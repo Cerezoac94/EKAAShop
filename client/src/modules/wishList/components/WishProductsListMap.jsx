731b3d28665201f1af0f967c5b00ed3c74d9a4cf
KevinConde <conde.jimenez@hotmail.com>
2023-03-12 19:23:10 -0600

    fix(client): Create-allproducts-in-wishList

---
+++ client/src/modules/wishList/components/WishProductsListMap.jsx
@@ -0,0 +1,19 @@
+import { useGetWishQuery } from "../../../redux/service/wish.service.jsx";
+import WishProductsList from "./WishProductsList.jsx";
+const WishListMap = ({ me }) => {
+  const { data: results, isLoading, error } = useGetWishQuery(me); 
+
+  return (
+      error ? (
+        <h1>{error?.data?.message}</h1>
+      ) : isLoading ? (
+        <h3>Cargando...</h3>
+      ) : (
+        (results?.results?.Products?.map((w) => <WishProductsList w={w} key={w.id}/>))
+      )
+
+    /* este compoente va mapear los productos de las listas (cooler o yetiss) */
+  );
+};
+
+export default WishListMap;
