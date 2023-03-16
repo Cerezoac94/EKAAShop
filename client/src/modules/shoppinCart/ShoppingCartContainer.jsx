3644df45ff7acc081fd1378cd6fecd0018a88186
Jorge Cerezo <antonio.cerezo94@hotmail.com>
2023-03-16 16:46:07 -0600

    fix(client-cart): Fix btn create order on cart

--- client/src/modules/shoppinCart/ShoppingCartContainer.jsx
+++ client/src/modules/shoppinCart/ShoppingCartContainer.jsx
@@ -1,11 +1,11 @@
-import CreateOrder from "./components/listCart/CreateOrder";
+// import CreateOrder from "./components/listCart/CreateOrder";
 import ListCartMap from "./components/listCart/ListCartMap";
 
 const ShoppingCartContainer = () => {
   return (
     <>
       <ListCartMap me={1} />
-      <CreateOrder me={1} />
+      {/* <CreateOrder me={1} /> */}
     </>
   );
 };
