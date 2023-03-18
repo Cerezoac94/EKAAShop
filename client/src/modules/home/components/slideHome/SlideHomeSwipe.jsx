6c8e8c7c6bc9b4fa0d6bb4234daac2eecd947486
KevinConde <conde.jimenez@hotmail.com>
2023-03-18 03:52:08 -0600

    fix(client-slidehome): fix-slide-home

--- client/src/modules/home/components/slideHome/SlideHomeSwipe.jsx
+++ client/src/modules/home/components/slideHome/SlideHomeSwipe.jsx
@@ -1,6 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
-import { useGetAllProductsQuery } from "../../../../redux/service/product.service";
+import { Link } from "react-router-dom";
+import { useGetAllProductsQuery} from "../../../../redux/service/product.service";
 import "swiper/scss";
 
 const SlideHomeSwipe = () => {
@@ -13,51 +13,33 @@ const SlideHomeSwipe = () => {
   ) : (
     <Swiper
       className="wiper"
-      // slidesPerView={3}
       spaceBetween={30}
-      // pagination={{
-      //   clickable: true,
-      // }}
-      // navigation={true}
+      loop={true}
     >
       {results?.results?.slice(0, 3).map((p) => {
         return (
           <SwiperSlide className="swiper" key={p.id}>
-            <article section className="slide">
+            <article className="slide">
               <section className="slide__nameProduct">
                 <section className="slide__name">
-                  <label className="slide__font">Set Yeti 20 Oz & </label>
-                  <label className="slide__font">Rambler colster 12 Oz</label>
+                  <label className="slide__font">{p.name}</label>
+                  {/* <label className="slide__font">Rambler colster 12 Oz</label> */}
                 </section>
 
-                <section>
-                  <button className="slide__btn">Comprar</button>
-                  <button className="slideHome__btn2">ver mas</button>
+                <section className="slide__botons">
+                  <button className="slide__btn">Add to Cart</button>
+                  <Link to={`/product_detail/${p.id}`}>
+                    <button className="slide__btn2">Ver mas</button>
+                  </Link>
                 </section>
               </section>
-              <img className="imgSlide" src={p.image} alt={p.name} />
+              <img className="slide__imgSlide" src={p.image} alt={p.name} />
             </article>
           </SwiperSlide>
         );
       })}
     </Swiper>
   );
-  /* <section>
-          <div className="slideHome">
-            <article>
-              <div className="slideHome__text">
-                <label className="">Set Yeti 20 Oz & </label>
-                <label htmlFor="">Rambler colster 12 Oz</label>
-              </div>
-
-              <div>
-                <button className="slideHome__btn">Comprar</button>
-                <button className="slideHome__btn2">ver mas</button>
-              </div>
-            </article>
-            <img src={img_cup_3} className="slideHome__img " alt="slide" />
-          </div>
-        </section> */
 };
 
 export default SlideHomeSwipe;
