2742ea3592abd7192b485e8918c7f8c12f1ea021
EugeniaRS <kenayanrkt@gmail.com>
2023-03-20 18:39:01 -0600

    refactor(cards):update-cards

--- client/src/modules/products/components/presentacional/Product.jsx
+++ client/src/modules/products/components/presentacional/Product.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import {Link} from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
 const ProductDrinkware = ({ product }) => {
-
+console.log(product)
   return (
 <Link to={`/product_detail/${product.id}`} className="product_item_container">
+<label className="product_item_name">{product.name}</label>
           <Image src={`${product.image}`}
             alt={product.name}
             className="product_image" />
-          <label className="product_item_name">{product.name}</label>
+            <label htmlFor=""> {product.Category.name}</label>
+           <label htmlFor="">{product.stock}</label>
           <label className="product_item_price">$ {product.price}</label>
 </Link>
-  );
+  ); 
 };
 
 export default ProductDrinkware;
