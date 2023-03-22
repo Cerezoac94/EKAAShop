577fe83f745924a75cc1057cf5b4284b1490738e
Jorge Cerezo <antonio.cerezo94@hotmail.com>
2023-03-22 12:17:35 -0600

    refactor(client-products): Refactor render if stock

--- client/src/modules/products/components/presentacional/Product.jsx
+++ client/src/modules/products/components/presentacional/Product.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-import Image from 'react-bootstrap/Image'
+import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
 const Product = ({ product }) => {
-  return (
-<Link to={`/product_detail/${product.id}`} className="product_item_container">
-<label className="product_item_name">{product.name}</label>
-          <Image src={`${product.image}`}
-            alt={product.name}
-            className="product_image" />
-            <label htmlFor=""> {product.Category.name}</label>
-           <label htmlFor="">{product.stock}</label>
-          <label className="product_item_price">$ {product.price}</label>
-</Link>
-  ); 
+	return product.stock > 0 && (
+		<Link
+			to={`/product_detail/${product.id}`}
+			className="product_item_container"
+		>
+			<label className="product_item_name">{product.name}</label>
+			<Image
+				src={`${product.image}`}
+				alt={product.name}
+				className="product_image"
+			/>
+			<label> {product.Category.name}</label>
+			<label>In stock</label>
+			<label className="product_item_price">$ {product.price}</label>
+		</Link>
+	);
 };
 
 export default Product;
