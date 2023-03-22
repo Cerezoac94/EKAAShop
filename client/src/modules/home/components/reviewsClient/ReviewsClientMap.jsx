6e44e9f4ac914cd0b6e18a6fb52cfde5e23875a2
KevinConde <conde.jimenez@hotmail.com>
2023-03-22 15:01:42 -0600

    fix(client slideReviws): fix-slideReviews-add-star

--- client/src/modules/home/components/reviewsClient/ReviewsClientMap.jsx
+++ client/src/modules/home/components/reviewsClient/ReviewsClientMap.jsx
@@ -1,16 +1,56 @@
-// import React from 'react'
-// import ReviewsClient from './ReviewsClient'
-// import {useGetAllReviewsQuery} from '../../../../redux/service/review.service'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import { useGetAllReviewsQuery } from "../../../../redux/service/review.service";
+// import "swiper/scss";
+import Loading from "../../../../components/loading/Loading";
+import ReviewsClient from "./ReviewsClient";
+const ReviewsClientMap = ({ e }) => {
+  const { data: results = [], isLoading, error } = useGetAllReviewsQuery();
 
-// const ReviewsClientMap = () => {
-// const { data: results = [], isLoading, error } = useGetAllReviewsQuery();
-// if(!error) {
-//   return isLoading ? (<h3> cargando</h3>) :   (
-//     results.length != 0 &&(results?.results.slice(0,1).map(e  => (<ReviewsClient e={e} key={e.id}/>))
-//     )
-//   )
+  if (!error) {
+    return isLoading ? (
+      <Loading />
+    ) : (
+      results.length != 0 && (
+        <Swiper
+            
+            slidesPerView={3}
+            spaceBetween={2}
+            breakpoints={{
+              "@0.00": {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              "@0.75": {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              "@1.00": {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              "@1.50": {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+            pagination={true}
+            navigation={true}
+            modules={[Navigation, Pagination]}
+            // centeredSlides={true}
+        
+        >
+          {results?.results?.slice(0, 10).map((e) => {
+            return (
+              <SwiperSlide key={e.id}>
+                <ReviewsClient r={e} />;
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      )
+    );
+  }
+};
 
-// }
-// } 
-
-// export default ReviewsClientMap
+export default ReviewsClientMap;
