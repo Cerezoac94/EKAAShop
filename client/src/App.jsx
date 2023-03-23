8db0aa9849d2255ff5a9c9241ab2fa81f599f49a
Jorge Cerezo <antonio.cerezo94@hotmail.com>
2023-03-23 15:41:00 -0600

    fix(client-app): Fix scrollRestoration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import Footer from "./components/footer/Footer.jsx"
 import Header from './components/header/Header.jsx'
 import Main from "./components/main/Main.jsx"
-// import { useMeQuery } from "./redux/service/session.service.jsx"
+import { ScrollRestoration } from "react-router-dom"
 const App = () => {
   // const { data, isLoading} = useMeQuery();
   // console.log(data);
   return (
     <>
+    <ScrollRestoration/>
     <Header/>
     <Main/>
     <Footer/>
