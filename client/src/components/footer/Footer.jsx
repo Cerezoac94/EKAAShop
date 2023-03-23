790b32890beaaf2524866049c55548f5f0493532
EugeniaRS <kenayanrkt@gmail.com>
2023-03-23 00:17:58 -0600

    fix(footer):refactorizacion de footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -7,22 +7,31 @@ const Footer = () => {
       <div className="footer__grupo1">
         <div className="footer__box">
           <figure>
-            <img src={logo} alt="logo_home" className="footer__logo" />
+          <Link to="/" className="logo_link_home">
+            <img src={logo} alt="logo_home" className="footer__logo" /> 
+          </Link>
           </figure>
         </div>
         <div className="footer__box">
-          <h2>ABOUT US</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
-            quidem.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
-            quidem.
-          </p>
+           <h2>Más información</h2> 
+          <ul>
+          <li className="footer__options">
+											<Link to="/about_us" >Acerca de nosotros</Link>
+										</li>
+          <li className="footer__options">
+											<Link to="/profile" >Perfil</Link>
+										</li>
+										<li className="footer__options">
+											<Link to="/orders">Pedidos</Link>
+										</li>
+										<li className="footer__options">
+											<Link to="/wish_list">Lista de deseos</Link>
+										</li>
+          </ul>
         </div>
         <div className="footer__box">
-          <h2>FOLLOW</h2>
+          <h2>Contactanos</h2>
+          <hr />
           <div className="footer__redSocial">
             <Link to="/">
               <ion-icon name="logo-facebook"></ion-icon>
