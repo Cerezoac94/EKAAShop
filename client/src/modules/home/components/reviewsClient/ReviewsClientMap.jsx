d858382fed31758f1a35341bfc159f81ead8d190
KevinConde <conde.jimenez@hotmail.com>
2023-03-22 20:05:13 -0600

    fix(client-slideReview): Fix-size-slide-review

--- client/src/modules/home/components/reviewsClient/ReviewsClientMap.jsx
+++ client/src/modules/home/components/reviewsClient/ReviewsClientMap.jsx
@@ -1,7 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 import { useGetAllReviewsQuery } from "../../../../redux/service/review.service";
-// import "swiper/scss";
 import Loading from "../../../../components/loading/Loading";
 import ReviewsClient from "./ReviewsClient";
 const ReviewsClientMap = () => {
@@ -13,37 +12,22 @@ const ReviewsClientMap = () => {
     ) : (
       results.length != 0 && (
         <Swiper
-            
-            slidesPerView={3}
-            spaceBetween={2}
-            breakpoints={{
-              "@0.00": {
-                slidesPerView: 1,
-                spaceBetween: 10,
-              },
-              "@0.75": {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              "@1.00": {
-                slidesPerView: 3,
-                spaceBetween: 40,
-              },
-              "@1.50": {
-                slidesPerView: 4,
-                spaceBetween: 50,
-              },
+          className="swiperReview"
+          slidesPerView={1}
+          breakpoints={
+            {1000:{
+              slidesPerView:2
             }}
-            pagination={true}
-            navigation={true}
-            modules={[Navigation, Pagination]}
-            // centeredSlides={true}
-        
+          }
+          pagination={true}
+          navigation={true}
+          modules={[Navigation, Pagination]}
+          // centeredSlides={true}
         >
           {results?.results?.slice(0, 10).map((e) => {
             return (
-              <SwiperSlide key={e.id}>
-                <ReviewsClient r={e} />;
+              <SwiperSlide  key={e.id}>
+                <ReviewsClient  r={e} />
               </SwiperSlide>
             );
           })}
