2559c9e22e28a659e4848920bc515afad47fe9e0
Jorge Cerezo <antonio.cerezo94@hotmail.com>
2023-03-23 23:49:38 -0600

    fix(client-keepBuyin): Fix margin bottom on keepBuying

--- client/src/modules/home/components/keepBuying/KeepBuyingMap.jsx
+++ client/src/modules/home/components/keepBuying/KeepBuyingMap.jsx
@@ -4,23 +4,22 @@ import KeepBuying from "./KeepBuying";
 
 const KeepBuyingMap = ({ me }) => {
 	const { data: results = [], isLoading, error } = useGetWishQuery(me);
-  if(!error) {
-    return isLoading ? (
-      <Loading/>
-    ) : (
-      results?.results.Products.length > 0 && (
-        <section>
-          <label className="keepBuying__label">Seguir Comprando</label>
-  
-          <section className="keepBuyingContainer">
-            {results?.results.Products.slice(0, 4).map((product) => (
-              <KeepBuying product={product} key={product.id} />
-            ))}
-          </section>
-        </section>
-      )
-    );
-  }
+	if (!error) {
+		return isLoading ? (
+			<Loading />
+		) : (
+			results?.results.Products.length > 0 && (
+				<section className="keepBuyingSection">
+					<label className="keepBuying__label">Seguir Comprando</label>
+					<section className="keepBuyingContainer">
+						{results?.results.Products.slice(0, 4).map((product) => (
+							<KeepBuying product={product} key={product.id} />
+						))}
+					</section>
+				</section>
+			)
+		);
+	}
 };
 
 export default KeepBuyingMap;
