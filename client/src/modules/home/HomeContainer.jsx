ff0f6aa79909facd97197d28197bcb81c91e5db3
EugeniaRS <kenayanrkt@gmail.com>
2023-03-24 09:49:24 -0600

    add(text):reseña

--- client/src/modules/home/HomeContainer.jsx
+++ client/src/modules/home/HomeContainer.jsx
@@ -16,7 +16,16 @@ const HomeContainer = () => {
       <Warranty/>
       <InterestYouSwiper/>
       <BannerOffersCont/>
-      <ReviewsClientMap/>
+      <div className="review">
+        <h2>Reseñas</h2>
+        <ion-icon color="warning" name="star"></ion-icon>
+        <ion-icon color="warning" name="star"></ion-icon>
+        <ion-icon color="warning" name="star"></ion-icon>
+        <ion-icon color="warning" name="star"></ion-icon>
+        <ion-icon color="warning" name="star"></ion-icon>
+        <ReviewsClientMap/>
+      </div>
+    
      { isLoading ? ( <Loading/>
      ) : ( !error && <KeepBuyingMap me={me.result.id}/> ) }
 </>
